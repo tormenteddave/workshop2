08e97da83b22cd605f0c1604eb74bdc7dc2451da
David Weiss <tormenteddave@gmail.com>
2025-12-26 19:27:52 -0700

    updated ip address in app/nc_tutorials/settings.py

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-(j-#aqq@vs$3l#0(031z4_h(_*pa&ye#dt&x69_@(&a6(=+fpg
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'ec2-3-21-13-147.us-east-2.compute.amazonaws.com', '0.0.0.0', 'localhost', '127.0.0.1'
+    'ec2-3-21-13-147.us-east-2.compute.amazonaws.com', '3.21.13.147', 'localhost', '127.0.0.1'
 ]
 
 

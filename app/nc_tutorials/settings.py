9543f58a4e89f401409ba48b1134ac6d651b93dc
David Weiss <tormenteddave@gmail.com>
2025-12-26 19:16:27 -0700

    updated app/nc_tutorials/settings.py, if/else statement was wrong

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 if DEBUG:
     STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-else
+else:
     STATIC_ROOT = [os.path.join(BASE_DIR, "static")]
 
 STATIC_URL = '/static/'
